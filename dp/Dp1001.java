6f0d620172054df98786496297c29b7c6b8691e3
melanieing <melanie0617@gmail.com>
2023-04-25 23:08:54 +0900

    10-01. 계단오르기

---
+++ dp/Dp1001.java
@@ -0,0 +1,38 @@
+package inflearnAlgorithm.dp;
+
+import java.util.Scanner;
+
+/**
+ * 10-01. 계단오르기
+ * 한 번에 한 계단 혹은 두 계단씩 올라가기
+ * 총 N 계단 (3 <= N <= 35)일 때, 올라가는 방법의 수 구하기
+ * 입력
+ * 7
+ * 출력
+ * 21
+ */
+public class Dp1001 {
+
+    static int[] dy;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Dp1001 main = new Dp1001();
+        int n = sc.nextInt();
+        dy = new int[n+1];
+        System.out.println(main.solution(n));
+    }
+
+    public int solution(int n) {
+
+        dy[1] = 1; // 계단이 하나일 때
+        dy[2] = 2; // 계단이 둘일 때
+
+        for (int i = 3; i <= n; i++) { // 계단이 셋 이상일 때
+            dy[i] = dy[i-2] + dy[i-1];
+        }
+
+        return dy[n];
+    }
+
+}
